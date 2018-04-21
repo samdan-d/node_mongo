130b3647665398ba2080f57dff245b1d0eb6d669
Samdan Dansranbavuu <pri.samdan@gmail.com>
2018-04-21 23:05:56 +0800

    Add tests for GET /todos

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,13 +4,28 @@ const request = require('supertest');
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
-beforeEach((done) => {
-    Todo.remove({}).then(() => done());
+const todos = [{
+    text: 'First test todo'
+}, {
+    text: 'Second test todo'
+}];
+
+beforeEach(function (done) {
+    this.timeout(2500);
+    Todo.remove({}).then(() => {
+        return Todo.insertMany(todos);
+    })
+        .then(() => {
+            done();
+        });
 });
 
-describe('POST / todos', function() {
+describe('POST /todos', function () {
     it('should create a new todo', (done) => {
+        this.timeout(2000);
         let text = 'Test todo text.';
+
+        console.log('Hey2');
 
         request(app)
             .post('/todos')
@@ -20,11 +35,11 @@ describe('POST / todos', function() {
                 expect(res.body.text).toBe(text);
             })
             .end((err, res) => {
-                if(err) {
+                if (err) {
                     return done(err);
                 }
 
-                Todo.find().then((todos) => {
+                Todo.find({text}).then((todos) => {
                     expect(todos.length).toBe(1);
                     expect(todos[0].text).toBe(text);
                     done();
@@ -38,14 +53,27 @@ describe('POST / todos', function() {
             .send({text: ' '})
             .expect(400)
             .end((err, res) => {
-                if(err) {
+                if (err) {
                     return done(err);
                 }
 
                 Todo.find().then((todos) => {
-                    expect(todos.length).toBe(0);
+                    expect(todos.length).toBe(2);
                     done();
                 }).catch((e) => done(e));
             })
     });
 });
+
+describe(('GET /todos'), () => {
+    it('should get all todos', (done) => {
+        request(app)
+            .get('/todos')
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todos.length).toBe(2);
+            })
+            .end(done);
+    })
+})
+;
