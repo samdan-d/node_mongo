deb9cc04885c99b68878d0ed0785676601abefa7
Samdan Dansranbavuu <pri.samdan@gmail.com>
2018-04-28 03:10:10 +0800

    Add POST /user route and token method

--- server/server.js
+++ server/server.js
@@ -42,7 +42,7 @@ app.get('/todos/:id', (req, res) => {
 
     // Validate Id using isValid
     // 404 - send back empty send
-    if(!ObjectID.isValid(id)) {
+    if (!ObjectID.isValid(id)) {
         res.status(404).send();
     }
     // findById
@@ -53,7 +53,7 @@ app.get('/todos/:id', (req, res) => {
     // 400 - and send empty body
     Todo.findById(id)
         .then((todo) => {
-            if(!todo) {
+            if (!todo) {
                 res.status(404).send();
             }
             res.send({todo});
@@ -64,21 +64,21 @@ app.get('/todos/:id', (req, res) => {
 
 app.delete('/todos/:id', (req, res) => {
     // Get the ID
-    let  id = req.params.id;
+    let id = req.params.id;
 
     // Validate the id -> not valid return 404
-    if(!ObjectID.isValid(id)) {
+    if (!ObjectID.isValid(id)) {
         return res.status(404).send();
     }
 
     // Remove todo by id
-        // Success
-            // if no doc, send 404
-            // if doc, send doc back with 200
-        // Error
-            // 400 with empty body
+    // Success
+    // if no doc, send 404
+    // if doc, send doc back with 200
+    // Error
+    // 400 with empty body
     Todo.findByIdAndRemove(id).then((todo) => {
-        if(!todo) {
+        if (!todo) {
             return res.status(404).send();
         }
         return res.send({todo});
@@ -91,19 +91,19 @@ app.patch('/todos/:id', (req, res) => {
     let id = req.params.id;
     let body = _.pick(req.body, ['text', 'completed']);
 
-    if(!ObjectID.isValid(id)) {
+    if (!ObjectID.isValid(id)) {
         return res.status(404).send();
     }
 
-    if(_.isBoolean(body.completed) && body.completed) {
+    if (_.isBoolean(body.completed) && body.completed) {
         body.completedAt = new Date().getTime()
     } else {
         body.completed = false;
         body.completedAt = null;
     }
 
-    Todo.findByIdAndUpdate(id, {$set: body},{new: true}).then((todo) => {
-        if(!todo) {
+    Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+        if (!todo) {
             return res.status(404).send();
         }
 
@@ -111,6 +111,23 @@ app.patch('/todos/:id', (req, res) => {
     }).catch((e) => {
         res.status(400).send();
     });
+});
+
+// Post /users
+app.post('/users', (req, res) => {
+    let info = _.pick(req.body, ['email', 'password']);
+    let user = User(info);
+
+    user.save()
+        .then((user) => {
+            return user.generateAuthToken();
+        })
+        .then((token) => {
+            res.header('x-auth', token).send(user);
+        })
+        .catch((e) => {
+            res.status(400).send(e);
+        });
 });
 
 app.listen(port, () => {
